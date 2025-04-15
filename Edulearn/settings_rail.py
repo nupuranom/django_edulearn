3bf28ec7287b188e2f6e41f6a17c9fcdbe8e6601
nupuranom <ummeanom@gmail.com>
2025-04-15 22:26:37 +0600

    Initial commit for deployment

---
+++ Edulearn/settings_rail.py
@@ -0,0 +1,5 @@
+from Edulearn.settings import *
+
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
--- Edulearn/wsgi.py
+++ Edulearn/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Edulearn.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Edulearn.settings_rail')
 
 application = get_wsgi_application()
